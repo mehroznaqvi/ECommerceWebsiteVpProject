cb59cd6cb65f2ca45547d6d30e6903ab276fa96a
Mehroz Naqvi <mehrozali1990@gmail.com>
2018-04-26 01:28:27 +0500

    Initial commit

---
+++ ECommerceWebsiteVpProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECommerceWebsiteVpProject.Startup))]
+namespace ECommerceWebsiteVpProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
