1e960c051655ddbaa1ce86c8a6c105dd2cee765a
Mehroz Naqvi <mehrozali1990@gmail.com>
2018-05-15 10:08:49 +0500

    Modify HTML page
    DataBase Connection

--- ECommerceWebsiteVpProject/Controllers/CartController.cs
+++ ECommerceWebsiteVpProject/Controllers/CartController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ECommerceWebsiteVpProject.Models;
 
 namespace ECommerceWebsiteVpProject.Controllers
 {
     public class CartController : Controller
     {
-        //
-        // GET: /Cart/
+        private product db = new product();
         public ActionResult Index()
         {
             return View();
---
+++ ECommerceWebsiteVpProject/OnlineShopData/DataContextInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ECommerceWebsiteVpProject.Models;
+
+namespace ECommerceWebsiteVpProject.OnlineShopData
+{
+    public class DataContextInitializer:DropCreateDatabaseIfModelChanges<OnlineShopData>
+    {
+        protected override void Seed(OnlineShopData context)
+        {
+            var products = new List<product>();
+            {
+                new product() { pName = "product11", pPrice = "1000", pDateCreation = "2018-22-6" };
+            };
+
+        }
+    }
+}
